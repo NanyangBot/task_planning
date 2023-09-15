cd1651d7744f17bafc8da22e79bf13ee48563886
Carlo Wiesse <wiessel@artc.a-star.edu.sg>
2023-09-15 10:03:24 +0800

    feature to save path message as YAML in debug node

--- wall_painting_trajectory_planner/wall_painting_trajectory_planner/trajectory_planner_node_debug.py
+++ wall_painting_trajectory_planner/wall_painting_trajectory_planner/trajectory_planner_node_debug.py
@@ -9,6 +9,32 @@ from task_msgs.msg import DistanceMapMsg
 from visualization_msgs.msg import MarkerArray
 from wall_painting_trajectory_planner.trajectory_planner import TrajectoryPlanner
 
+import yaml
+
+def save_path_message_as_yaml(msg):
+    # msg_dict = {field: (getattr(msg, field) if isinstance(getattr(msg, field), str) else float(getattr(msg, field))) for field in msg._fields_and_field_types if hasattr(msg, field)}
+
+    pose_dict_list = []
+    for p in msg.poses:
+        x = float(p.pose.position.x)
+        y = float(p.pose.position.y)
+        z = float(p.pose.position.z)
+        qx = float(p.pose.orientation.x)
+        qy = float(p.pose.orientation.y)
+        qz = float(p.pose.orientation.z)
+        qw = float(p.pose.orientation.w)
+        frame_id = p.header.frame_id
+        pose_dict = {'header':{'frame_id':frame_id},
+                     'pose':{'position':{'x':x, 'y':y, 'z':z},
+                             'orientation':{'x':qx, 'y':qy, 'z':qz, 'w':qw}}}
+        pose_dict_list.append(pose_dict)
+    frame_id = msg.header.frame_id
+    dict_file = {'header':{'frame_id':frame_id}, 'poses':pose_dict_list}
+
+    # Save path as a YAML file in the location from where you're running this node
+    with open('output_file.yaml', 'w') as file:
+        out = yaml.dump(dict_file, file)
+
 ###############################################################################
 
 class TrajectoryPlannerNode(Node):
@@ -61,6 +87,8 @@ class TrajectoryPlannerNode(Node):
 
         self.planner.plan_task(self.task)
         path = self.planner.get_path()
+
+        save_path_message_as_yaml(path)
         self.path_pub.publish(path)
 
         self.busy = False
